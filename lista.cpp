3c0109c110a68d82a8d28153f7e6514b90302119
jonjon667 <jonjon667@hotmail.com>
2019-03-26 23:03:00 -0300

    Implementacao de parte da lista
    implementadas inserção e busca

---
+++ lista.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+struct Node{
+	string data;
+	int nSearch;
+	struct Node* prev;
+	struct Node* next;
+};
+
+class List{
+	public:
+	
+	struct Node* head = NULL;
+	struct Node* last = NULL;
+	bool search(string data) { //TODO organizar a lista a cada busca
+		int pos = 0;
+	   
+	   if(head==NULL) {
+		  return false;
+	   } 
+	   struct Node* current = head;
+	   while(current!=NULL) {
+		  if(current->data == data) { //caso encontre o dado, aumentar o numero de vezes 
+			 current->nSearch +=1;
+			 return true;
+		  }
+
+		 else if(current->next != NULL)
+			 current = current->next;
+		  else
+			 break;
+	   }
+		return false;
+	}
+
+	void push(string newdata) { 
+		struct Node* newnode = (struct Node*) malloc(sizeof(struct Node)); 
+		newnode->data  = newdata; 
+		newnode->prev = NULL; 
+	  	newnode->next = head;     
+		if(head !=  NULL) head->prev = newnode;     
+		head = newnode; 
+	}
+	void print(){
+		struct Node* aux;
+		aux = head;
+		while(aux!=NULL){
+			cout<<aux->data<<" "<<aux->nSearch<<endl;
+			aux = aux->next;
+		}
+	}
+	
+};
+
+
+void swap(Node* a, Node* b) 
+{ 
+    Node* temp = a; 
+    a = b; 
+    b = temp; 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
